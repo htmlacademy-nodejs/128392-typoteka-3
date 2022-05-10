2e3a4f49f9723cba0daacf2a34f7ba2e7fbda6ee
stradzhu.d <stradzhu.d@gmail.com>
2022-05-11 01:08:45 +0300

    Моки читаются из файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,19 +3,23 @@
 const chalk = require(`chalk`);
 const util = require(`util`);
 const fs = require(`fs`);
-const {getRandomInt, shuffle} = require(`../../utils`);
-const {CATEGORIES, DEFAULT_COUNT, MAX_COUNT, SENTENCES, TITLES, FILE_NAME, TimeInMilliseconds} = require(`../../constants`);
+const {getRandomInt, shuffle, readFileToArray} = require(`../../utils`);
+const {DEFAULT_COUNT, MAX_COUNT, FILE_NAME, TimeInMilliseconds} = require(`../../constants`);
 const {ExitCode} = require(`../../constants`);
 
-const generateOffers = (count) => (
-  Array.from({length: count}, () => ({
+const generateOffers = async (count) => {
+  const CATEGORIES = await readFileToArray(`categories.txt`);
+  const TITLES = await readFileToArray(`titles.txt`);
+  const SENTENCES = await readFileToArray(`sentences.txt`);
+
+  return Array.from({length: count}, () => ({
     title: TITLES[getRandomInt(0, TITLES.length - 1)],
     createdDate: new Date(Date.now() - getRandomInt(0, TimeInMilliseconds.THREE_MONTHS)),
     announce: shuffle(SENTENCES).slice(0, getRandomInt(1, SENTENCES.length - 1)).join(` `),
     fullText: shuffle(SENTENCES).slice(0, getRandomInt(1, SENTENCES.length - 1)).join(` `),
     category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-  }))
-);
+  }));
+};
 
 module.exports = {
   name: `--generate`,
@@ -27,13 +31,14 @@ module.exports = {
       process.exit(ExitCode.ERROR);
     }
 
-    const content = JSON.stringify(generateOffers(countOffer), null, 4);
+    const content = JSON.stringify(await generateOffers(countOffer), null, 4);
 
     try {
-      await util.promisify(fs.writeFile)(`../....../${FILE_NAME}`, content);
+      await util.promisify(fs.writeFile)(`../../${FILE_NAME}`, content);
       console.info(chalk.green(`Готово. Файл ${FILE_NAME} успешно создан.`));
     } catch (err) {
       console.error(chalk.red(`Не удается записать данные в файл...`));
+      console.error(err);
     }
   }
 };
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const fs = require(`fs`);
+const chalk = require(`chalk`);
+const util = require(`util`);
+
 module.exports.getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -14,4 +18,23 @@ module.exports.shuffle = (someArray) => {
   }
 
   return someArray;
+};
+
+module.exports.readFileToArray = async (filename) => {
+  let lines = [];
+
+  try {
+    const buffer = await util.promisify(fs.readFile)(`../../data/${filename}`);
+    lines = buffer.toString().split(`\n`);
+
+    if (lines[lines.length - 1] === ``) {
+      lines = lines.slice(0, -1);
+    }
+  } catch (err) {
+    console.error(chalk.red(`Не удается прочитать файл ${filename}`));
+    console.error(err);
+    process.exit(1);
+  }
+
+  return lines;
 };
