62b76598d4ec4fdd9354de1554d37d2ff6b0c465
stradzhu.d <stradzhu.d@gmail.com>
2022-07-20 21:47:00 +0300

    замена сервера http на express

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const chalk = require(`chalk`);
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
 
 const {DEFAULT_PORT, FILE_NAME, HttpCode, notFoundMessageText} = require(`../../constants`);
@@ -11,49 +11,26 @@ module.exports = {
   run(args) {
     const customPort = Number.parseInt(args[0], 10) || DEFAULT_PORT;
 
-    const sendResponse = (res, statusCode, message) => {
-      const template = `
-        <!Doctype html>
-          <html lang="ru">
-            <head>
-              <title>With love from Node</title>
-            </head>
-            <body>${message}</body>
-          </html>`.trim();
+    const app = express();
+    app.use(express.json());
 
-      res.writeHead(statusCode, {
-        'Content-Type': `text/html; charset=UTF-8`,
-      });
-
-      res.end(template);
-    };
-
-    const onClientConnect = async (req, res) => {
-      switch (req.url) {
-        case `/`:
-          try {
-            const fileContent = await fs.readFile(`${FILE_NAME}`);
-            const mocks = JSON.parse(fileContent);
-            const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-            sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-          } catch (err) {
-            sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-          }
-          break;
-        default:
-          sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-          break;
+    app.get(`/posts`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILE_NAME);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (_err) {
+        res.send([]);
       }
-    };
+    });
 
-    http.createServer(onClientConnect)
-      .listen(customPort)
-      .on(`listening`, () => {
-        console.info(chalk.green(`Ожидаю соединений на ${customPort}`));
-      })
-      .on(`error`, ({message}) => {
-        console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
-      });
+    app.use((req, res) => res.status(HttpCode.NOT_FOUND).send(notFoundMessageText));
 
+    app.listen(customPort, (err) => {
+      if (err) {
+        console.error(chalk.red(err.message));
+      }
+      console.info(chalk.green(`Сервер успешно запущен на порту ${customPort}`));
+    });
   }
 };
